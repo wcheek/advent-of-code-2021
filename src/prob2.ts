45a9c99017f252fb27c658153664993bb52ff45c
Wesley Cheek <66931618+wcheek@users.noreply.github.com>
2022-09-14 13:55:51 +0900

    working on prob2

---
+++ src/prob2.ts
@@ -0,0 +1,57 @@
+import { readFileSync, promises as fsPromises } from "fs";
+import { join } from "path";
+
+// ✅ read file ASYNCHRONOUSLY
+async function asyncReadFile(filename: string) {
+  try {
+    const result = await fsPromises.readFile(
+      join(__dirname, filename),
+      "utf-8"
+    );
+
+    // console.log(result); // 👉️ "hello world hello world ..."
+
+    // console.log(result.split(" "));
+    return result;
+  } catch (err) {
+    console.log(err);
+    return "Something went wrong";
+  }
+}
+
+const instructions = asyncReadFile("../assets/prob2_input.txt");
+
+class Position {
+  x: number;
+  y: number;
+  constructor(x = 0, y = 0) {
+    this.x = x;
+    this.y = y;
+  }
+  moveForward(n: number) {
+    this.x += n;
+  }
+  moveVertically(n: number) {
+    this.y += n;
+  }
+}
+let subPosition = new Position();
+
+instructions
+  .then((result) => result.split("\n").map(String))
+  .then((result) => {
+    result.forEach((value: string, index: number, array: string[]) => {
+      const [instruction, amount] = value.split(" ") as [string, number];
+    //   console.log(instruction, amount);
+      if (instruction == "forward") {
+        subPosition.moveForward(amount);
+      } else if (instruction == "up") {
+        subPosition.moveVertically(amount);
+      } else if (instruction == "down") {
+        subPosition.moveVertically(-1 * amount);
+      }
+    });
+  });
+
+console.log(subPosition)
+
