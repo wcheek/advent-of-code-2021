fe93ab81a5211d67a11f0209f93edf35e2869f94
Wesley Cheek <66931618+wcheek@users.noreply.github.com>
2022-09-20 16:08:40 +0900

    Hmm figuring out indexing is hard

--- src/prob4.ts
+++ src/prob4.ts
@@ -1,6 +1,6 @@
 import { readFileSync, promises as fsPromises } from "fs";
 import { join } from "path";
-const numbers: number[] = [
+const bingoNumbers: number[] = [
   31, 50, 68, 16, 25, 15, 28, 80, 41, 8, 75, 45, 96, 9, 3, 98, 83, 27, 62, 42,
   59, 99, 95, 13, 55, 10, 23, 84, 18, 76, 87, 56, 88, 66, 1, 58, 92, 89, 19, 54,
   85, 74, 39, 93, 77, 26, 30, 52, 69, 48, 91, 73, 72, 38, 64, 53, 32, 51, 6, 29,
@@ -39,15 +39,19 @@ let getNumberRow = function (value: string): number[] {
   });
   return numbers;
 };
+
 bingoCardsInput
   .then((result) => result.split("\n").map(String))
   .then((result) => {
     result.forEach((value: string, index: number, array: string[]) => {
       let numbers = getNumberRow(value);
       if (numbers.length != 1) {
-        console.log(Math.floor(index / 6));
+        // console.log(index)
+        // console.log(Math.floor(index / 6));
+        console.log(numbers)
+        console.log(bingoCards[0])
         bingoCards[Math.floor(index / 6)].bingoArray[index % 6] = numbers;
       }
     });
-    console.log(bingoCards[1]);
+    // console.log(bingoCards[9]);
   });
