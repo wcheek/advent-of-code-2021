54ea952b5ed53c373282272400acfcb1dafbc042
Wesley Cheek <66931618+wcheek@users.noreply.github.com>
2022-09-21 11:54:21 +0900

    using synchronous file loading instead

--- src/prob4.ts
+++ src/prob4.ts
@@ -1,4 +1,4 @@
-import { readFileSync, promises as fsPromises } from "fs";
+import { readFileSync } from "fs";
 import { join } from "path";
 const bingoNumbers: number[] = [
   31, 50, 68, 16, 25, 15, 28, 80, 41, 8, 75, 45, 96, 9, 3, 98, 83, 27, 62, 42,
@@ -7,20 +7,15 @@ const bingoNumbers: number[] = [
   17, 90, 34, 61, 70, 4, 7, 57, 44, 97, 82, 37, 43, 14, 81, 65, 11, 22, 5, 36,
   71, 35, 78, 12, 0, 94, 47, 49, 33, 79, 63, 86, 40, 21, 24, 46, 20, 2, 67, 60,
 ];
+// ✅ read file SYNCHRONOUSLY
+function syncReadFile(filename: string) {
+  const result = readFileSync(join(__dirname, filename), "utf-8");
 
-// ✅ read file ASYNCHRONOUSLY
-async function asyncReadFile(filename: string) {
-  try {
-    const result = await fsPromises.readFile(
-      join(__dirname, filename),
-      "utf-8"
-    );
-    return result;
-  } catch (err) {
-    console.log(err);
-    return "Something went wrong";
-  }
+  console.log(result); // 👉️ "hello world hello world ..."
+
+  return result;
 }
+
 class bingoCard {
   numberArray: number[][];
   cardNumber: number;
@@ -29,31 +24,38 @@ class bingoCard {
     this.cardNumber = cardNumber;
   }
 }
-let bingoCards: bingoCard[] = [];
-for (let i = 0; i < 100; i++) {
-  bingoCards.push(new bingoCard([], i));
-}
-// let bingoCards: bingoCard[] = Array(100).fill(new bingoCard([], ));
-let bingoCardsInput = asyncReadFile("../assets/prob4_input.txt");
+class bingoGame {
+  bingoCards: bingoCard[];
+  constructor() {
+    this.getCards();
+  }
+  getCards() {
+    let bingoCards: bingoCard[] = [];
+    for (let i = 0; i < 100; i++) {
+      bingoCards.push(new bingoCard([], i));
+    }
+    let bingoCardsInput = syncReadFile("../assets/prob4_input.txt");
 
-let getNumberRow = function (value: string): number[] {
-  let newValue = value[0] == " " ? value.slice(1) : value;
-  let numbers: number[] = [];
-  let doubleSplit = newValue.split("  ");
-  doubleSplit.forEach((value) => {
-    numbers = [...numbers, ...value.split(" ").map(Number)];
-  });
-  return numbers;
-};
+    let getNumberRow = function (value: string): number[] {
+      let newValue = value[0] == " " ? value.slice(1) : value;
+      let numbers: number[] = [];
+      let doubleSplit = newValue.split("  ");
+      doubleSplit.forEach((value) => {
+        numbers = [...numbers, ...value.split(" ").map(Number)];
+      });
+      return numbers;
+    };
 
-bingoCardsInput
-  .then((result) => result.split("\n").map(String))
-  .then((result) => {
-    result.forEach((value: string, index: number, array: string[]) => {
+    let resultArray = bingoCardsInput.split("\n").map(String);
+    resultArray.forEach((value: string, index: number, array: string[]) => {
       let numbers = getNumberRow(value);
       if (numbers.length != 1) {
         bingoCards[Math.floor(index / 6)].numberArray[index % 6] = numbers;
       }
     });
-    console.log(bingoCards[99]);
-  });
+    this.bingoCards = bingoCards;
+  }
+}
+
+let game = new bingoGame();
+console.log(game.bingoCards[0]);
