898b8d33b201b0f8cf7d6edacc451fe789824476
Wesley Cheek <66931618+wcheek@users.noreply.github.com>
2022-09-22 15:37:07 +0900

    filtered input

--- src/prob5.ts
+++ src/prob5.ts
@@ -4,15 +4,54 @@ import { join } from "path";
 function syncReadFile(filename: string) {
   return readFileSync(join(__dirname, filename), "utf-8");
 }
-function getInput(): number[][][] {
-  let input = syncReadFile("../assets/prob5_input.txt");
-  let inputArray = input.split("\n").map(String);
-  let xyPairs: number[][][] = inputArray.map((coords) => {
-    let tup = coords.split(" -> ").map(String);
-    return [tup[0].split(",").map(Number), tup[1].split(",").map(Number)];
-  });
-  return xyPairs;
+
+class Input {
+  private input: number[][][];
+  filteredInput: number[][][];
+  constructor() {
+    this.input = this.getInput();
+    this.filteredInput = this.filterInput();
+  }
+
+  private getInput(): number[][][] {
+    let input = syncReadFile("../assets/prob5_input.txt");
+    let inputArray = input.split("\n").map(String);
+    let xyPairs: number[][][] = inputArray.map((coords) => {
+      let tup = coords.split(" -> ").map(String);
+      return [tup[0].split(",").map(Number), tup[1].split(",").map(Number)];
+    });
+    return xyPairs;
+  }
+  private filterInput(): number[][][] {
+    let filteredInput: number[][][] = [];
+    for (const coords of this.input) {
+      if (coords[0][0] === coords[1][0] || coords[0][1] === coords[1][1]) {
+        filteredInput.push(coords);
+      }
+    }
+    return filteredInput;
+  }
 }
 
-let input = getInput();
-console.log(input);
+class Field extends Input {
+  private field: number[][];
+  constructor() {
+    super();
+    this.field = this.createEmptyField();
+    this.fillField();
+  }
+
+  private createEmptyField(): number[][] {
+    // 1000 X 1000 field of 0s
+    return Array(1000).fill(Array(1000).fill(0));
+  }
+
+
+  fillField() {
+    for (const coords of this.filteredInput) {
+
+    }
+  }
+}
+
+let field = new Field();
