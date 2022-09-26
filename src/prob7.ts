38edea257c7426bb1ca815746babb50bc27e5564
Wesley Cheek <66931618+wcheek@users.noreply.github.com>
2022-09-26 16:43:19 +0900

    part1 soln

--- src/prob7.ts
+++ src/prob7.ts
@@ -4,15 +4,29 @@ function syncReadFile(filename: string) {
   return readFileSync(join(__dirname, filename), "utf-8");
 }
 import { readFileSync } from "fs";
-class Input {
-  protected input: number[];
-  constructor() {
-    this.input = this.getInput();
-  }
 
-  private getInput(): number[] {
-    let input = syncReadFile("../assets/prob6_input.txt");
-    let inputArray = input.split(",").map(Number);
-    return inputArray;
+function getInput(): number[] {
+  let input = syncReadFile("../assets/prob7_input.txt");
+  let inputArray = input.split(",").map(Number);
+  return inputArray;
+}
+
+function getUniqueVals(array: number[]): number[] {
+  return array.filter((value: number, index: number, self: number[]) => {
+    return self.indexOf(value) === index;
+  });
+}
+
+let input = getInput();
+var uniqueVals = getUniqueVals(input);
+
+let totalFuel: number[] = Array(uniqueVals.length).fill(0)
+for (let uniqueValInd in uniqueVals) {
+  for (let ind in input) {
+    totalFuel[uniqueValInd] += Math.abs(input[ind] - uniqueVals[uniqueValInd]);
   }
 }
+
+let smallestFuelInd = totalFuel.indexOf(Math.min(...totalFuel))
+let smallestLoc = uniqueVals[smallestFuelInd]
+console.log(Math.min(...totalFuel))
