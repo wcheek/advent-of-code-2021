602c10b6e743189f5be3167293338aa9b080bef0
Wesley Cheek <66931618+wcheek@users.noreply.github.com>
2022-09-26 16:54:53 +0900

    organize for part2

--- src/prob7.ts
+++ src/prob7.ts
@@ -1,9 +1,8 @@
 import { join } from "path";
-// ✅ read file SYNCHRONOUSLY
+import { readFileSync } from "fs";
 function syncReadFile(filename: string) {
   return readFileSync(join(__dirname, filename), "utf-8");
 }
-import { readFileSync } from "fs";
 
 function getInput(): number[] {
   let input = syncReadFile("../assets/prob7_input.txt");
@@ -11,22 +10,19 @@ function getInput(): number[] {
   return inputArray;
 }
 
-function getUniqueVals(array: number[]): number[] {
-  return array.filter((value: number, index: number, self: number[]) => {
-    return self.indexOf(value) === index;
-  });
+function getTotalFuel(uniqueVals: number[]) {
+  let totalFuel: number[] = Array(uniqueVals.length).fill(0);
+  for (let uniqueValInd in uniqueVals) {
+    for (let ind in input) {
+      totalFuel[uniqueValInd] += Math.abs(
+        input[ind] - uniqueVals[uniqueValInd]
+      );
+    }
+  }
+  return totalFuel;
 }
 
 let input = getInput();
-var uniqueVals = getUniqueVals(input);
-
-let totalFuel: number[] = Array(uniqueVals.length).fill(0)
-for (let uniqueValInd in uniqueVals) {
-  for (let ind in input) {
-    totalFuel[uniqueValInd] += Math.abs(input[ind] - uniqueVals[uniqueValInd]);
-  }
-}
-
-let smallestFuelInd = totalFuel.indexOf(Math.min(...totalFuel))
-let smallestLoc = uniqueVals[smallestFuelInd]
-console.log(Math.min(...totalFuel))
+var uniqueVals = [...new Set(input)];
+let totalFuel = getTotalFuel(uniqueVals);
+console.log(Math.min(...totalFuel));
